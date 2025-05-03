c39aca804334c554dd2afd9ee3122257c7e96f6c
aurelienrubillon <aurelien.rubillon@edu.ece.fr>
2025-05-03 16:58:28 +0200

    Update Badland.h

--- Badland.h
+++ Badland.h
@@ -1,3 +1,5 @@
+
+
 #ifndef BADLAND_H
 #define BADLAND_H
 
@@ -8,34 +10,23 @@
 #define SCENE_WIDTH 13500
 #define DECOR_SCROLL_SPEED 3
 #define GRAVITY 1
-#define JUMP_STRENGTH -10
+#define JUMP_STRENGTH -11
 #define MAX_NAME_LENGTH 30
-#define MAX_OBSTACLES 100
 #define SAVE_FILE "save.txt"
 
 typedef struct {
     int x, y;
     int dx, dy;
-    int tx, ty;
-    int couleur;
-    int width, height;
-} Acteur;
-
-typedef struct {
-    int x, y;
-    int dx, dy;
-    int width, height;
-    char name[MAX_NAME_LENGTH];
+    int largeur, hauteur;
+    char nom[MAX_NAME_LENGTH];
     int niveau;
 } Joueur;
 
+void initialisation_allegro();
 void init_jeu(Joueur *joueur);
-void charger_joueur(Joueur *joueur);
-void sauvegarder_joueur(Joueur *joueur);
-void generer_obstacles(Acteur *obstacles, int *nb);
-void update_jeu(Joueur *joueur, Acteur *obstacles, int nb, int decor_scroll);
-void draw_jeu(Joueur *joueur, BITMAP *page, int decor_scroll, Acteur *obstacles, int nb, BITMAP *sprite);
-int collisionActeurs(Acteur *a, Acteur *b);
-int menu(BITMAP *page);
+void jeu_scrolling(const char *pseudo);
+void draw_jeu(Joueur *joueur, BITMAP *page, int decor_scroll, BITMAP *sprite);
+void menu_principal();
 
-#endif
+
+#endif //BADLAND_H
