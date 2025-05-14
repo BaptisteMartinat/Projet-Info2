187a059576b375d798d09abeb202901a92a84eb6
VictorfrgECE <victor.frege@edu.ece.fr>
2025-05-14 13:16:00 +0200

    Update Badland.h

--- Badland.h
+++ Badland.h
@@ -23,19 +23,22 @@ typedef struct {
 } Joueur;
 
 #define NB_CHECKPOINTS 4
-
+#define NB_ROUES 1
 typedef struct {
     int x, y;
     int actif;     // 1 = activé, 0 = non
 } Checkpoint;
-
+typedef struct {
+    int x, y;
+    int frame;      // index de l'image (0 à 3)
+} Roue;
 void initialisation_allegro();
 void init_jeu(Joueur *joueur);
 void jeu_scrolling(const char *pseudo);
 void draw_jeu(Joueur *joueur, BITMAP *page, int decor_scroll, BITMAP *sprite);
 void menu_principal();
 void menu_selection_map(const char *pseudo);
-
+int detecter_arrivee(BITMAP *map, int cx, int cy, int rayon, int scroll);
 int joueur_sur_checkpoint(Joueur *joueur, int cp_x, int cp_y, int scroll, int sprite_w, int sprite_h);
 void init_checkpoints(Checkpoint checkpoints[NB_CHECKPOINTS]);
 
